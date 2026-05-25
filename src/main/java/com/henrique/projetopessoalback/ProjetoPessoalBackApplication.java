1bdb22de6eb64a665c683bbaf486a4bb0ef7ae2f
Henrique <henrique.malafaia@gmail.com>
2026-05-25 15:07:45 -0300

    initialize Spring Boot backend project with Maven wrapper

---
+++ src/main/java/com/henrique/projetopessoalback/ProjetoPessoalBackApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.projetopessoalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPessoalBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoPessoalBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/henrique/projetopessoalback/ProjetoPessoalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.projetopessoalback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPessoalBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
